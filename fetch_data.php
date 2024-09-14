d92aeb341a27f375c22da9118c39c1482279ce65
Thomasjg123 <thomasgoltz18@gmail.com>
2024-09-13 20:26:01 -0500

    prints one line in sql

--- fetch_data.php
+++ fetch_data.php
@@ -3,25 +3,26 @@
 
 // Database connection
 $host = 'mysql';
-$dbname = 'messages';
+$dbname = 'copier';
 $username = 'root';
 $password = 'password123';
-/* 
+
 $conn = new mysqli($host, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} */
+}
 
 
-//$sql = "SELECT * FROM videos";
-//$result = $conn->query($sql);
-//$row = $result->fetch_assoc()
+$sql = "SELECT * FROM messages";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+echo $row['message'];
 
-// then to pull use the row with this asthe alias
+ //then to pull use the row with this asthe alias
 // $videoName = $row['description'];
 
-try {
+/* try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -31,7 +32,7 @@ try {
     echo json_encode($data);
 } catch (PDOException $e) {
     ec/ho json_encode(['error' => $e->getMessage()]);
-}
+} */
 
 
 $conn->close();
