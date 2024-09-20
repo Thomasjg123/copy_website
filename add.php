86663220eb503bf65e8e042f1b37683f69cc13ce
Thomasjg123 <thomasgoltz18@gmail.com>
2024-09-19 20:50:15 -0500

    add a button

---
+++ add.php
@@ -0,0 +1,39 @@
+<?
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ $text = $_POST['message'];
+  
+  $host = 'mysql';
+$dbname = 'copier';
+$username = 'root';
+$password = 'password123';
+
+echo $text;
+
+$text = 'Hello world from php';
+
+
+// Create a connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+
+}
+ 
+    //This is the start of putting things in database
+ 
+      $sql = "INSERT INTO messages (message) VALUES '$text'";
+
+      echo $videoDesc;
+      if ($conn->query($sql) === TRUE) {
+        echo "Your message was added";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+  } else {
+      echo "Sorry, there was an error uploading your file.";
+  }
+
+  ?>
